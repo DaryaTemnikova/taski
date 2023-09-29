498e6a752379914331e26043f9461687a6ea3232
root <root@epdb0vrkakdnlqab8imm.auto.internal>
2023-09-29 15:46:02 +0000

    make work

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,26 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+sentry_sdk.init(
+    # В этой переменной будет значение для вашего проекта.
+    dsn="https://566e05253a108eb59803cc45edca7bd5@o4505899728306176.ingest.sentry.io/4505899734466560",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.77.221', '127.0.0.1', 'localhost', 'important-taski.myftp.org']
 
 
 # Application definition
@@ -103,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/urls.py
+++ backend/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, include
 from rest_framework import routers
 from api import views
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
+    path('sentry-debug/', trigger_error), # Новый маршрут.
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
